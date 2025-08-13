4cd883b6b8144aefc71ab01c4cf088ec03e71903
Ameerah Razali <wanameerahfatini@gmail.com>
2025-08-13 18:23:13 +0800

    Update whatsapp-engagement-analysis.py

--- whatsapp-engagement-analysis.py
+++ whatsapp-engagement-analysis.py
@@ -1,4 +1,4 @@
-# Q1: For WhatsApp groups with more than 50 participants that were created in October 2024, what is the average number of messages sent? 
+### Q1: For WhatsApp groups with more than 50 participants that were created in October 2024, what is the average number of messages sent? 
 # This insight will help assess engagement in larger groups and support recommendations for group messaging features.
 
 # for a group more than 50 in oct 2024
@@ -13,7 +13,7 @@ avg_messages = filtered_groups['total_messages'].mean()
 print(f"The average number messages sent for filtered group is : {avg_messages}")
 
 
-# Q2: What is the average number of participants in WhatsApp groups that were created in October 2024? 
+### Q2: What is the average number of participants in WhatsApp groups that were created in October 2024? 
 # This number will indicate the typical group size and inform our group messaging feature considerations.
 
  # Filter for groups created in October 2024
@@ -26,7 +26,7 @@ avg_participants = oct_groups['participant_count'].mean()
 print(f"The average number of participants in groups created in October 2024 is: {avg_participants}")
 
 
-# Q3: For WhatsApp groups with more than 50 participants that were created in October 2024, what is the average number of messages sent?
+### Q3: For WhatsApp groups with more than 50 participants that were created in October 2024, what is the average number of messages sent?
 # This insight will help assess engagement in larger groups and support recommendations for group messaging features.
 
 # for a group more than 50 in oct 2024
